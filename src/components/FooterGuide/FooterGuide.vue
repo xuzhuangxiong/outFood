6f58e5a46061adc7e01a5f24a709ca97172a69df
无敌的自恋狂 <1046960832@qq.com>
2020-02-26 14:19:18 +0800

    完成底部导航栏

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,20 +1,86 @@
 <template>
-  <div >
-      foot
-  </div>
+  <footer class="footer_guide border-1px">
+    <div class="guide_item" @click="goto('/msite')"  :class="{on: isCurrent('/msite')}"  >
+      <span class="item_icon">
+        <i class="iconfont iconziyuan"></i>
+      </span>
+      <span>外卖</span>
+    </div>
+
+    <div class="guide_item" @click="goto('/search')" :class="{on: isCurrent('/search')}">
+      <span class="item_icon">
+        <i class="iconfont iconsousuo"></i>
+      </span>
+      <span>搜索</span>
+   </div>
+    <div class="guide_item" @click="goto('/order')" :class="{on: isCurrent('/order')}">
+      <span class="item_icon">
+        <i class="iconfont icondingdan"></i>
+      </span>
+      <span>订单</span>
+    </div>
+   <div class="guide_item" @click="goto('/profile')" :class="{on: isCurrent('/profile')}">
+      <span class="item_icon">
+        <i class="iconfont icongeren2"></i>
+      </span>
+      <span>我的</span>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {
-  
-  data() { 
-    return {
-
+  data() {
+    return {};
+  },
+  methods: {
+    goto(path){
+       this.$router.replace(path)
+    },
+    isCurrent(path){
+      return this.$route.path===path
     }
-  }
- }
+  },
+};
 </script>
 
-<style lang="stylus" scoped>
- 
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixins.styl';
+
+.footer_guide { // footer
+  top-border-1px(#e4e4e4);
+  position: fixed;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  width: 100%;
+  height: 50px;
+  display: flex;
+
+  .guide_item {
+    display: flex;
+    flex: 1;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    margin: 5px;
+    color: #999999;
+
+    &.on {
+      color: #02a774;
+    }
+
+    span {
+      font-size: 12px;
+      margin-top: 2px;
+      margin-bottom: 2px;
+
+      .iconfont {
+        font-size: 22px;
+      }
+    }
+  }
+}
 </style>
